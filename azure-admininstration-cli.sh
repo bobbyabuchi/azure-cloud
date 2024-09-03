92755c4711d2e3606b019547cfce3567c35fd6c7
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-09-03 15:39:41 +0100

    Update azure-admininstration-cli.sh

--- azure-admininstration-cli.sh
+++ azure-admininstration-cli.sh
@@ -1,2 +1,5 @@
 # list the names of all subnets contained within all vnets
 az network vnet list --query [].subnets[].name
+
+# change the next-hop IP address whithin an existing route table
+az network route-table route update
