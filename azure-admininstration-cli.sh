f44de11a923b373079540220955dd8b2665a827c
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-09-24 13:44:09 +0100

    Update azure-admininstration-cli.sh

--- azure-admininstration-cli.sh
+++ azure-admininstration-cli.sh
@@ -3,3 +3,6 @@ az network vnet list --query [].subnets[].name
 
 # change the next-hop IP address whithin an existing route table
 az network route-table route update
+
+# create a windows virtual machine with arm template
+az deployment group create --name <deployment-name> --template-file <path-to-template-file> --parameters '{"adminUsername": {"value": "<admin-username>"}, "adminPassword": {"value": "<admin-password>"}}' --resource-group <resource-group-name>
