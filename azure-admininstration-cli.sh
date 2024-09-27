7c8349724bcdd07d932e7645c7791fa066cacf68
bobbyabuchi <bobbyabuchi@gmail.com>
2024-09-27 14:37:14 +0100

    updated azure-administration-cli.sh

--- azure-admininstration-cli.sh
+++ azure-admininstration-cli.sh
@@ -1,8 +1,31 @@
-# list the names of all subnets contained within all vnets
-az network vnet list --query [].subnets[].name
-
-# change the next-hop IP address whithin an existing route table
-az network route-table route update
+#..COMPUTE....................................................
 
 # create a windows virtual machine with arm template
-az deployment group create --name <deployment-name> --template-file <path-to-template-file> --parameters '{"adminUsername": {"value": "<admin-username>"}, "adminPassword": {"value": "<admin-password>"}}' --resource-group <resource-group-name>
+az deployment group create --name <deployment-name> \
+    --template-file <path-to-template-file> \
+    --parameters '{"adminUsername": {"value": "<admin-username>"}, "adminPassword": {"value": "<admin-password>"}}' \
+    --resource-group <resource-group-name>
+
+#..move a VM to a different resource group, subscription & region
+
+# deallocate the resources
+az vm deadallocate --name myVM \
+    --resource-group oldResourceGroup
+# create the new resource group if none exists
+az group create --name newResourceGroup \
+    --location newRegion
+# move the VM to new resource group
+az vm create --name myVM \
+    --resource-group newResourceGroup\
+    --location newRegion \
+    --source oldResourceGroup
+az vm wait --name myVM\
+    --resource-group oldResourceGroup --deleted
+
+#..NETWORK....................................................
+
+#..STORAGE....................................................
+
+#..MONITOR....................................................
+
+#..IAM....................................................
